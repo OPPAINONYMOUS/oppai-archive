86487d1d2b2f0e91f6763d76ba4b7d2e066aaf84
oppainonymous <116291596+OPPAINONYMOUS@users.noreply.github.com>
2024-03-10 07:24:34 +0800

    Add files via upload

---
+++ openclash-autoreboot/run-autoreboot.sh
@@ -0,0 +1,6 @@
+cd /usr/bin
+rm -r watchcat.sh
+wget https://raw.githubusercontent.com/OPPAINONYMOUS/oppai-archive/main/openclash-autoreboot/watchcat.sh
+chmod +x watchcat.sh
+cd
+service watchcat restart
